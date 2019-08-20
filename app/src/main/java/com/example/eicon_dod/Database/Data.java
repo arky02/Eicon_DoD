3f834c02812abbffefe19cdb88ce99186af314d8
iammarkps <markkung08@gmail.com>
2019-08-20 17:17:32 +0900

    Record bad word

--- app/src/main/java/com/example/eicon_dod/Database/Data.java
+++ app/src/main/java/com/example/eicon_dod/Database/Data.java
@@ -5,15 +5,22 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 
 @Entity(tableName = "data")
 public class Data {
     @PrimaryKey
-    public int uid;
+    public String uuid;
 
     @ColumnInfo(name = "time")
     public Timestamp timestamp;
 
     @ColumnInfo(name = "word")
     public String word;
+
+    public Data(Timestamp timestamp, String word) {
+        this.uuid = UUID.randomUUID().toString();
+        this.timestamp = timestamp;
+        this.word = word;
+    }
 }
