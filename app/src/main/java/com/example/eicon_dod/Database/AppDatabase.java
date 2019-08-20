9b95ff4f95e9ef2da56695f96794e478fe3a77a6
iammarkps <markkung08@gmail.com>
2019-08-20 18:44:51 +0900

    Fix main thread query

--- app/src/main/java/com/example/eicon_dod/Database/AppDatabase.java
+++ app/src/main/java/com/example/eicon_dod/Database/AppDatabase.java
@@ -7,6 +7,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
+
 @Database(entities = {Data.class}, version = 1, exportSchema = false)
 @TypeConverters({TimestampConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
@@ -14,10 +15,11 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "main").build();
+            instance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "main").allowMainThreadQueries().build();
         }
         return instance;
     }
 
     public abstract DataDAO dataDAO();
 }
+
